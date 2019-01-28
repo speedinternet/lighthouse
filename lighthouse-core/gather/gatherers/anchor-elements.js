4f1f1b58141a52dfd762272ba6b35f7ed742a6af
Patrick Hulce <patrick.hulce@gmail.com>
2019-01-28 11:56:25 -0600

    we just need typechecking

--- lighthouse-core/gather/gatherers/anchor-elements.js
+++ lighthouse-core/gather/gatherers/anchor-elements.js
@@ -8,6 +8,51 @@
 const Gatherer = require('./gatherer.js');
 const pageFunctions = require('../../lib/page-functions.js');
 
+/**
+ * Function that is stringified and run in the page to collect anchor elements.
+ * Additional complexity is introduced because anchors can be HTML or SVG elements.
+ * @return {LH.Artifacts['AnchorElements']}
+ */
+/* istanbul ignore next */
+function collectAnchorElements() {
+  /** @param {string} url */
+  const resolveURLOrEmpty = url => {
+    try { return new URL(url, window.location.href).href; }
+    catch (_) { return ''; }
+  };
+
+  /** @type {Array<HTMLAnchorElement|SVGAElement>} */
+  // @ts-ignore - put into scope via stringification
+  const anchorElements = getElementsInDocument('a');
+
+  return anchorElements.map(node => {
+    /** @type {HTMLAnchorElement} */
+    const anchorNode = (node);
+    /** @type {LH.Artifacts.AnchorElement} */
+    const anchorElementInfo = {
+      href: anchorNode.href,
+      text: anchorNode.innerText,
+      rel: anchorNode.rel,
+      target: anchorNode.target,
+      // @ts-ignore - put into scope via stringification
+      outerHTML: getOuterHTMLSnippet(node),
+    }
+
+    /** @type {string|SVGAnimatedString} */
+    const href = (anchorNode.href);
+    /** @type {SVGAElement} */
+    const svgNode = (node);
+    if (href instanceof SVGAnimatedString) {
+      anchorElementInfo.href = resolveURLOrEmpty(href.baseVal);
+      anchorElementInfo.text = svgNode.textContent || '';
+      anchorElementInfo.rel = '';
+      anchorElementInfo.target = svgNode.target.baseVal || '';
+    }
+
+    return anchorElementInfo;
+  });
+}
+
 class AnchorElements extends Gatherer {
   /**
    * @param {LH.Gatherer.PassContext} passContext
@@ -18,35 +63,14 @@ class AnchorElements extends Gatherer {
     const expression = `(() => {
       ${pageFunctions.getOuterHTMLSnippetString};
       ${pageFunctions.getElementsInDocumentString};
-      const resolveURLOrEmpty = url => {
-        try { return new URL(url, window.location.href).href; }
-        catch (_) { return ''; }
-      };
 
-      return getElementsInDocument('a').map(node => ({
-        href: node.href instanceof SVGAnimatedString ?
-          resolveURLOrEmpty(node.href.baseVal) :
-          node.href,
-        text: node.href instanceof SVGAnimatedString ?
-          node.textContent :
-          node.innerText,
-        rel: node.rel,
-        target: node.target,
-        outerHTML: getOuterHTMLSnippet(node),
-      }));
+      return (${collectAnchorElements})();
     })()`
 
     // We'll use evaluateAsync because the `node.getAttribute` method doesn't actually normalize
     // the values like access from JavaScript does.
     /** @type {Array<LH.Artifacts.AnchorElement>} */
-    const anchors = await driver.evaluateAsync(expression, {useIsolation: true});
-
-    for (const anchor of anchors) {
-      anchor.rel = anchor.rel.toLowerCase();
-      anchor.target = anchor.target.toLowerCase();
-    }
-
-    return anchors;
+    return driver.evaluateAsync(expression, {useIsolation: true});
   }
 }
 
